417064922728e6fad4f9fdbef5977f464db9ba3c
Nelson Reis <82620017+NelsonReisSousa@users.noreply.github.com>
2024-09-15 13:26:05 -0300

    Atualizar o script.js

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -14,17 +14,17 @@ form.addEventListener('submit', function(event){
 
     document.getElementById('infos').classList.remove('hidden');
 
-    if (bmi < 18.5) {
+    if (bmi <= 18.5) {
         description = 'Tá magrelo, engorda aí raquítico!';
-    } else if (bmi < 18.5 && bmi <= 25){
+    } else if (bmi <= 25){
         description = 'Boa meu fi! tá pampa!';
         value.classList.remove('attention');
         value.classList.add('normal');
-    } else if (bmi < 25 && bmi <= 30){
+    } else if (bmi <= 30){
         description = 'Cuidado! Tá ficando cheio de pamonha!';
-    } else if (bmi < 30 && bmi <= 35){
+    } else if (bmi <= 35){
         description = 'Tá um pouquinho obeso, te cuida cara de tabaco';
-    } else if (bmi < 35 && bmi <= 40){
+    } else if (bmi <= 40){
         description = 'Tá querendo morrer, com essa obesidade severa?';
     } else {
         description = 'Tá loko? Tá quase caindo duro de obesidade';
